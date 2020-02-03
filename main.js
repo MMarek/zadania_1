82c406b67bf361a101daa6edffa2894d35c258c3
MMarek <mildnermarek@gmail.com>
2020-02-03 13:14:03 +0100

    adding a product

--- main.js
+++ main.js
@@ -32,3 +32,44 @@ console.log(bubbleSort([1, 0, 28, 17, 62]));
 console.log(bubbleSort([33, 20, 18, 327, 12, 2]));
 console.log(bubbleSort([64, 240, 8, 0, 64, -1, -4]));
 console.log(bubbleSort([56, -140, -8, 0, 83, -42, 5, -1.6]));
+
+// zad 3
+
+// IIFE - Immediately-invoked function expression =  funkcja samowykonująca się w JavaScript.
+
+const shopModule = (function () {
+    const _products = [];
+
+    const _countNewPrice = (price) => {
+        return price + (price * 0.5) - 0.1;
+    };
+
+    const addProduct = (product) => {
+        const newPrice = _countNewPrice(product.price);
+         const newProduct = Object.assign({}, product, {price: newPrice});
+        _products.push(newProduct);
+    };
+    const showProducts = () => {
+        console.log(_products);
+    };
+
+    return {
+        addProduct,
+        showProducts
+    }
+
+})();
+
+const product1 = {
+    name: 'słuchawki',
+    price: 200
+};
+
+const product2 = {
+    name: 'buty',
+    price: 300
+};
+
+shopModule.addProduct(product1);
+shopModule.addProduct(product2);
+shopModule.showProducts();
